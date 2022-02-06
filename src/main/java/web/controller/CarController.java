f9a5e0bad1828cff80c131679e6b4a1b0efbbf9a
Maria <mashytkamickey@mail.ru>
2022-02-06 21:44:57 +0300

    .

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,12 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.Model.Car;
 import web.Service.CarServise;
 import web.Service.CarServiseImpl;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController {
